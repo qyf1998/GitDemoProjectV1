d9dde6e522ee8f97e9169c903d5907991eda9125
Yifan Qin <qin.yif@northeatern.edu>
2023-12-27 15:23:32 -0500

    push the file

---
+++ WebdriverTest/src/test/java/example/NewTest.java
@@ -0,0 +1,30 @@
+package example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	private WebDriver driver;
+	@Test
+	public void testEasy() {
+		driver.get("http://demo.guru99.com/test/guru99home/");
+		String title = driver.getTitle();
+		Assert.assertTrue(title.contains("Demo Guru99 Page"));
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		driver = new FirefoxDriver();
+		//System.setProperty("webdriver.gecko.driver", "C:\\SeleniumWebdrivers\\geckodriver.exe");
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+}
---
+++ WebdriverTest/src/test/java/example/NewTestChrome.java
@@ -0,0 +1,31 @@
+package example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTestChrome {
+	private WebDriver driver;
+	@Test
+	public void testEasy() {
+		driver.get("http://demo.guru99.com/test/guru99home/");
+		String title = driver.getTitle();
+		Assert.assertTrue(title.contains("Demo Guru99 Page"));
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		driver = new ChromeDriver();
+		//System.setProperty("webdriver.gecko.driver", "C:\\SeleniumWebdrivers\\geckodriver.exe");
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+}
